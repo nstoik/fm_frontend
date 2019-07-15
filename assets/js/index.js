ddd1f34390e19b8e6fcc4b89088a2caa06a4adcd
Nelson Stoik <nelsonstoik@gmail.com>
2019-07-14 22:55:57 -0600

    Upadated dependencies

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
