8193bb221eb77e13e95de94b3dd9147dacba338f
Nelson Stoik <nelsonstoik@gmail.com>
2021-02-13 21:27:01 -0700

    update npm config

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,21 @@ const webpack = require('webpack');
  * Webpack Plugins
  */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const IgnoreEmitPlugin = require('ignore-emit-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const ProductionPlugins = [
+  // production webpack plugins go here
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify("production")
+    }
+  })
+]
 
 // take debug mode from the environment
 const debug = (process.env.NODE_ENV !== 'production');
-const hashType = debug ? '[hash]': '[contentHash]'
-
-// Development asset host (webpack dev server)
-console.log('debug value is: ' + debug)
-const publicHost = debug ? 'http://127.0.0.1:2992' : '';
-
 const rootAssetPath = path.join(__dirname, 'assets');
+const env_mode = process.env.NODE_ENV
 
 module.exports = {
   // configuration
@@ -29,121 +28,97 @@ module.exports = {
     app: './assets/js/index',
     main_js: './assets/js/main',
     main_css: [
-      path.join(__dirname, 'node_modules', '@fortawesome', 'fontawesome-free', 'css', 'fontawesome.css'),
-      path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
-      path.join(__dirname, 'node_modules', 'malihu-custom-scrollbar-plugin', 'jquery.mCustomScrollbar.css'),
       path.join(__dirname, 'assets', 'css', 'style.css'),
       path.join(__dirname, 'assets', 'css', 'nav-style.css'),
     ],
+    vendor_css: [
+      path.join(__dirname, 'node_modules', '@fortawesome', 'fontawesome-free', 'css', 'all.css'),
+      path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
+      path.join(__dirname, 'node_modules', 'malihu-custom-scrollbar-plugin', 'jquery.mCustomScrollbar.css'),
+    ]
+  },
+  mode: env_mode,
+  watch: debug,
+  // could not get watch working without adding poll. Probably due to using docker with Windows and WSL2
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+    ignored: ['/node_modules/', '__pycache__']
   },
   output: {
-    path: path.join(__dirname, 'fm_frontend', 'static'),
-    publicPath: `${publicHost}/static/`,
-    filename: "js/[name]." + hashType + ".js",
-    chunkFilename: "js/[name]." + hashType + ".chunk.js"
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-          test: /\.js(\?.*)?$/i,
-          sourceMap: true,
-      }),
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: require('cssnano'),
-        cssProcessorPluginOptions: {
-          preset: ['default', { discardComments: { removeAll: true } }],
-        },
-        canPrint: true
-      }),
-    ],
+    chunkFilename: "[id].js",
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "fm_frontend", "static", "build"),
+    publicPath: "/static/build/",
   },
   resolve: {
     extensions: ['.js', '.jsx', '.css', '.vue'],
     alias:{
-      img: path.join(__dirname, 'assets', 'img'),
       'vue$': 'vue/dist/vue.esm.js'
     },
   },
-  devtool: 'source-map',
-  devServer: {
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    publicPath: `${publicHost}/static/`,
-    public: `${publicHost}`
-  },
+  devtool: debug ? "eval-source-map" : false,
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].bundle.css" }),
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+    new VueLoaderPlugin(),
+  ].concat(debug ? [] : ProductionPlugins),
   module: {
     rules: [
-      { 
-        test: /\.html$/,
-        loader: 'raw-loader' 
-      },
       {
         test: /\.vue$/,
         loader: 'vue-loader'
       },
       { 
         test: /\.less$/,
-        use: [MiniCssExtractPlugin.loader,'css-loader!less-loader' ] ,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader!less-loader' 
+        ],
+      },
+      { 
+        test: /\.html$/,
+        loader: 'raw-loader' 
       },
       {
         test: /\.css$/i,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: debug,
-            },
           },
           'css-loader',
-          {
-            loader: 'postcss-loader'
-          }
         ],
       },
       { 
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: 'url-loader', options: {limit: 10000, mimetype: 'application/font-woff' }
       },
       { 
         test: /\.(ttf|eot|svg|png|jpe?g|gif|ico)(\?.*)?$/i,
-        loader: `file-loader?context=${rootAssetPath}&name=[path][name].[hash].[ext]`
+        loader: 'file-loader',
+        options: {
+          context: rootAssetPath,
+          name: '[path][name].[ext]'
+        }
       },
       { 
         test: /\.js$/,
-        exclude: file => (
-          /node_modules/.test(file) &&
-          !/\.vue\.js/.test(file)
-        ),
+        exclude: /node_modules/,
         loader: 'babel-loader',
         options: { 
-          presets: ['env'],
-          plugins: ['transform-object-rest-spread'],
+          presets: ["@babel/preset-env"],
           cacheDirectory: true
         } 
       },
+      {
+        // see https://github.com/webpack/webpack/issues/11467
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false
+        }
+      },
     ],
   },
-  plugins: [
-    new IgnoreEmitPlugin(/(?<=main_css\s*).*?(?=\s*js)/gs),
-    new MiniCssExtractPlugin({ filename: 'css/[name].' + hashType + '.css', }),
-    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
-    new VueLoaderPlugin(),
-    new ManifestPlugin(
-      {
-          map: (file) => {
-          // Remove hash in manifest key
-          file.name = file.name.replace(/(\.[a-f0-9]{32})(\..*)$/, '$2');
-          return file;
-          },
-          writeToFileEmit: true,
-      }),
-  ].concat(debug ? [] : [
-    // production webpack plugins go here
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      }
-    }),
-    new CleanWebpackPlugin(),
-  ]),
 };
